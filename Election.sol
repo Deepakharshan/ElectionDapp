6d90a6c1c93760fa18df53a46c0c6950ca079bc0
Deepakharshan <94448252+Deepakharshan@users.noreply.github.com>
2021-11-22 15:35:11 +0530

    Create Election.sol

---
+++ Election.sol
@@ -0,0 +1,29 @@
+//SPDX-License-Identifier:UNLICENSED
+
+pragma solidity ^0.8.9;
+
+
+contract Election{
+    
+    uint candidateOneVotes;
+    uint candidateTwoVotes;
+    
+    mapping(address  => bool) voters;
+    
+    
+    function candidateOne()public{
+        require(!voters[msg.sender]);
+        voters[msg.sender] = true;
+        candidateOneVotes++;
+    }
+    
+    function candidateTwo()public{
+        require(!voters[msg.sender]);
+        voters[msg.sender] = true;
+         candidateTwoVotes++;
+    }
+    
+    function checkVote()public view returns(bool){
+        return voters[msg.sender];
+    }
+}
